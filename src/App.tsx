72a1338d6d1e03caca19864a09d2fcc7f4925bc2
[dyad] <topboykayz@gmail.com>
2026-05-11 23:55:41 +0100

    [dyad] Added QueryClientProvider to wrap the entire app, fixing the 'No QueryClient set' error by providing a react-query client to all components that use useQuery. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Index from "@/pages/Index";
 import Dashboard from "@/pages/Dashboard";
 import Login from "@/pages/Login";
 import NotFound from "@/pages/NotFound";
 import EditEvent from "@/pages/EditEvent";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/edit-event/:id" element={<EditEvent />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/edit-event/:id" element={<EditEvent />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 export default App;
